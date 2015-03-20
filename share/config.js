64577c51d05455cf79fda9cb6a0623b6dc6fbe82
pdeng <pdeng@thinkjoy.cn>
2015-03-20 17:15:24 +0800

    1.2

---
+++ share/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by pdeng on 2015/3/20.
+ */
+var CONTSTANT_URL ={
+    credit:'http://172.16.130.106:8086/credit'
+}
--- task/js/task-list.js
+++ task/js/task-list.js
@@ -1,46 +1,45 @@
-
 var Ajax = {
     getTaskList: function (token, queryTime, callback) {
+        jQuery.support.cors = true;
         $.ajax({
-            type: "get",
-            url: "../task/data/credit/task/getTaskList.json",
+            type: "post",
+            url: CONTSTANT_URL.credit + '/task/getTaskList',
             dataType: 'json',
             data: {
-                "style": "",
-                "data": {
-                    "token": token,
-                    "queryTime": queryTime
-                },
-                "clientInfo": {}
+                "token": token,
+                "queryTime": queryTime
             },
+
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
             success: callback
         });
     }
 };
 
 
-
 var UI = {
-    content:$("#content")
+    content: $("#content")
 };
 
-var goPage = function(){
+var goPage = function () {
 
 };
 
 
 $(function () {
-    var token;
-    var queryTime;
+    var token = '7079090b3146ecf3d0cb1ba1c0f87baf';
+    var queryTime = 0;
     Ajax.getTaskList(token, queryTime, function (res) {
         if (res.rtnCode == '0000000') {
-            var html = template('task-tem',res);
+            var html = template('task-tem', res);
             UI.content.html(html);
         } else {
             alert(res.msg);
         }
     });
-
 
 
 });
